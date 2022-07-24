fdbb10776ade17f4b83ef6659293afc39ab00cae
Nilva <piresnilva@gmail.com>
2022-07-23 22:15:08 -0300

    criei o codigo medicModel

--- src/models/medicModels.js
+++ src/models/medicModels.js
@@ -1,3 +1,58 @@
 const mongoose = require("mongoose")
-// aula tereza esquema
-const medicSchema = mongoose.Schema
+
+const medicSchema = mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: mongoose.Schema.Types.ObjectId
+    },
+
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    specialty: {
+        type: String,
+        required: true
+    },
+
+    attendance: {
+        type: String,
+        required: true
+    },
+
+    healt_unit: {
+        type: String,
+        required: true
+    },
+
+    surgery: {
+        type: String,
+        required: true
+    },
+
+    contact: {
+        type: Number,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    state:{
+        type: String,
+        required: true
+    },
+}, {timestamps: true})
+
+const Model = mongoose.model('medic', medicSchema)
+
+module.exports = Model
