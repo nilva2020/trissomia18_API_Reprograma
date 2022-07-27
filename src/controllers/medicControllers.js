cb9d2be4dccc777c4c992b08df8c517c91aa7e42
Nilva <piresnilva@gmail.com>
2022-07-27 11:36:52 -0300

    finalizei as rotas e testei no postman

--- src/controllers/medicControllers.js
+++ src/controllers/medicControllers.js
@@ -5,11 +5,27 @@ const MedicModels = require("../models/medicModels.js");
 const createMedic = async (req, res) => {
   try {
     const {
-      name, specialty, attend_type, healt_unit, surgery, contact, email, city, state,
+      name,
+      specialty,
+      attend_type,
+      healt_unit,
+      surgery,
+      contact,
+      email,
+      city,
+      state,
     } = req.body;
 
     const newMedic = new MedicModels({
-      name, specialty, attend_type, healt_unit, surgery, contact, email, city, state,
+      name,
+      specialty,
+      attend_type,
+      healt_unit,
+      surgery,
+      contact,
+      email,
+      city,
+      state,
     });
 
     const savedMedic = await newMedic.save();
@@ -31,18 +47,47 @@ const getAllMedic = async (req, res) => {
 };
 
 const findMedicById = async (req, res) => {
-  try{
-    const findMedic = await MedicModels.find();
-    res.status(200).json(findAllMedic);
+  try {
+    const findMedic = await MedicModels.findById(req.params.id);
+    res.status(200).json(findMedic);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
   }
-  }
+};
 
+const updateMedic = async (req, res) => {
+  try {
+    const { name, specialty, attend_type, healt_unit } = req.body;
+    const updateMedic = await MedicModels.findByIdAndUpdate(req.params.id, {
+      name,
+      specialty,
+      attend_type,
+      healt_unit,
+    });
+    res.status(200).json(updateMedic);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteMedic = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const delete_medic = await MedicModels.findByIdAndDelete(id);
+    const message = `Medic ${id} sucess delete`;
+    res.status(200).json({ message });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   createMedic,
   getAllMedic,
-  findMedicById
+  findMedicById,
+  updateMedic,
+  deleteMedic,
 };
--- src/routes/medicRoutes.js
+++ src/routes/medicRoutes.js
@@ -5,5 +5,8 @@ const myrouter = express.Router()
 myrouter.post("/medic", myControl.createMedic)
 myrouter.get("/allmedic", myControl.getAllMedic)
 myrouter.get("/medic/:id", myControl.findMedicById)
+myrouter.patch("/medic/:id", myControl.updateMedic)
+myrouter.delete("/medic/:id", myControl.deleteMedic)
+
 
 module.exports = myrouter
